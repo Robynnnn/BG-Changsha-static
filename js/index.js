13b8a10cff84e1607cb88cdb4415d06e0bb8c7be
罗培斌 <luopeibin@banggood.com>
2017-12-26 14:36:37 +0800

    修改首页样式

--- js/index.js
+++ js/index.js
@@ -2,24 +2,24 @@
  * @Author: Robyn 
  * @Date: 2017-12-19 11:32:28 
  * @Last Modified by: Robyn
- * @Last Modified time: 2017-12-19 21:57:35
+ * @Last Modified time: 2017-12-24 02:00:06
  */
 
 
 $(function () {
 
   //        tab栏效果
-  $('.tab-item').mouseover(function () {
-    //            上面选项卡的切换：其实就是排他思想
-                $(this).addClass('active').siblings().removeClass('active');
-    //            tab内容切换：其实也是排他，不过他需要和选项卡有对应关系
-    //            通过获取当前的索引值，让上下内容对应起来
-                var index = $(this).index();
-                $('.main').eq(index).addClass('selected').siblings().removeClass('selected');
-            });
-  $('.nav-in').mouseout(function () {
-    $('.main').removeClass('selected');
-    })
+  // $('.tab-item').mouseover(function () {
+  //   //            上面选项卡的切换：其实就是排他思想
+  //               $(this).addClass('active').siblings().removeClass('active');
+  //   //            tab内容切换：其实也是排他，不过他需要和选项卡有对应关系
+  //   //            通过获取当前的索引值，让上下内容对应起来
+  //               var index = $(this).index();
+  //               $('.main').eq(index).addClass('selected').siblings().removeClass('selected');
+  //           });
+  // $('.nav-in').mouseout(function () {
+  //   $('.main').removeClass('selected');
+  //   })
   
   // 动态返回按钮
   // 1.获取产品展示的高度
