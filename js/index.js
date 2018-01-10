4c8c2120aa0312db7e6f976f583413cd3d51ad87
罗培斌 <luopeibin@banggood.com>
2018-01-10 11:04:27 +0800

    首页加载更多特效

--- js/index.js
+++ js/index.js
@@ -2,42 +2,8 @@
  * @Author: Robyn 
  * @Date: 2017-12-19 11:32:28 
  * @Last Modified by: Robyn
- * @Last Modified time: 2018-01-10 10:00:45
+ * @Last Modified time: 2018-01-10 10:53:31
  */
-
-// 函数封装
-;(function ($) {
-  // 加载更多的特效
-  $.fn.loadMore = function () {
-    var addHeight       = 2*($(".productsShow li").height()+80),
-        proLiLength     = $(".productsShow li").length,
-        proUlSumHeight  =  $(".productsShow").height(),
-        currentUlHeight = addHeight;
-    // 判断是否隐藏loadmore
-    proLiLength <= 10 ?  $(".loadMore").css({"display":"none"}) :  $(".loadMore").css({"display":"block"});
-
-    if (proUlSumHeight > addHeight) {
-      $(".productsShow").css({"height":currentUlHeight});
-    }
-  
-    $(".loadMore").click(function () {
-  
-      currentUlHeight += addHeight;
-      console.log(addHeight);
-      $(".productsShow").css({"height":currentUlHeight});
-  
-      if (currentUlHeight >= proUlSumHeight ) {
-        
-        $(".productsShow").css({"height":proUlSumHeight});
-        $(".loadMore").css({"display":"none"});
-      } else {
-        $(".loadMore").css({"display":"block"});
-      }
-      });
-    };
-  // end
-  })(jQuery);
-
 
 $(function () {
 
@@ -61,6 +27,36 @@ $(function () {
   });
   // 返回顶部按钮区域结束
 
-  // 加载更多
-  $(".productsShow").loadMore();
+  // // 加载更多
+// start
+(function () {
+  var showMoreNChildren = function ($children, n) {
+    //显示某jquery元素下的前n个隐藏的子元素
+    var $hiddenChildren = $children.filter(":hidden");
+    var cnt = $hiddenChildren.length;
+    for (var i = 0; i < n && i < cnt; i++) {
+      $hiddenChildren.eq(i).show();
+    }
+    return cnt - n; //返回还剩余的隐藏子元素的数量
+  }
+
+  //对页中现有的class=showMorehandle的元素，在之后添加显示更多条，并绑定点击行为
+  $(".showMoreNChildren").each(function () {
+    var pagesize = $(this).attr("pagesize") || 10;
+    var $children = $(this).children();
+    if ($children.length > pagesize) {
+      for (var i = pagesize; i < $children.length; i++) {
+        $children.eq(i).hide();
+      }
+
+      $("<div class='loadMoreBox df-jcai-center'><div class='loadMore'><span class='loadMoreC'>Load More +</span></div></div>").insertAfter($(this)).click(function () {
+        if (showMoreNChildren($children, pagesize) <= 0) {
+          //如果目标元素已经没有隐藏的子元素了，就隐藏“点击更多的按钮条”
+          $(this).hide();
+        };
+      });
+    }
+  });
+})();
+  // end
 })
