8bcd107b38cd29b66acb7648428620fb0564dfd5
罗培斌 <luopeibin@banggood.com>
2018-01-16 10:48:18 +0800

    产品详情页右侧增加判断是否有poa属性

--- js/productListDeatil.js
+++ js/productListDeatil.js
@@ -2,7 +2,7 @@
  * @Author: Robyn 
  * @Date: 2017-12-26 16:38:19 
  * @Last Modified by: Robyn
- * @Last Modified time: 2018-01-15 18:11:49
+ * @Last Modified time: 2018-01-16 10:23:42
  */
 
 // 函数封装
@@ -35,57 +35,97 @@
 
   // 右侧color点击左侧展示对应大图
   $.fn.colorTurnBigImg = function (moveLength) {
-    $(".typeColor").on("click","li",function () {
+    $(".typeColor").on("click", "li", function () {
       // 获取右侧img中src的属性
       var imgSrc = $(this).find("img").attr("src");
       // 判断是否有图片，如果没有图片，就直接return 
       if (!imgSrc) {
-        return ;
+        return;
       }
       var li_node = $("#ban_num1 ul").find("li");
-      $.each(li_node,function (idx,ele) {
+      var rightSrc;
+      $.each(li_node, function (idx, ele) {
+        rightSrc = $(ele).find("img").attr("src");
         // 判断右侧的src和左侧小图哪个src相等，如果相等，就让当前li增加class on，其余移除on
-        if (imgSrc == $(ele).find("img").attr("src")) {
+        if (imgSrc == rightSrc) {
           // pc端每次移动505px
-          var moveDistance = -(idx*moveLength);
+          var moveDistance = -(idx * moveLength);
           $(this).addClass("on").siblings().removeClass("on");
-          $("#ban_pic1 ul").css({"left":moveDistance});
+          $("#ban_pic1 ul").css({
+            "left": moveDistance
+          });
         }
-        });
-        // end
       });
+      // end
+    });
 
     // end
-    };
+  };
 
+  $.fn.judgeRightHeight = function () {
+    var poaHeight = $(".poa").height(),
+      rightsellHeight = $(".rightSell").height();
+
+    if (!poaHeight) {
+      $(".rightSell").css({
+        "height": rightsellHeight + 95
+      });
+    } else if (poaHeight != 95) {
+      console.log(111);
+      if (poaHeight == 44) {
+        $(".rightSell").css({
+          "height": rightsellHeight + 51
+        });
+      } else if (poaHeight == 51) {
+        $(".rightSell").css({
+          "height": rightsellHeight + 44
+        });
+      }
+    }
+
+  };
 
   // 加载更多文字
   $.fn.readMore = function () {
-    
+
     var desBoxLength = $('.desBox').length;
     for (var i = 0; i < desBoxLength; i++) {
       // 判断文字高度
-      if ( $('.desBox').eq(i).height() >= 48) {
-        $('.readMore').eq(i).css({"display":"block"});
+      if ($('.desBox').eq(i).height() >= 48) {
+        $('.readMore').eq(i).css({
+          "display": "block"
+        });
       } else {
-        $('.readMore').eq(i).css({"display":"none"});
-        $('.hideBtn').eq(i).css({"display": "none"});
+        $('.readMore').eq(i).css({
+          "display": "none"
+        });
+        $('.hideBtn').eq(i).css({
+          "display": "none"
+        });
       }
     }
     // readMore按钮操作
     $('.readMore').click(function () {
-      $(this).css({"display":"none"});
-      $(this).siblings().removeClass('block-with-text').find('.hideBtn').css({"display":"inline-block"});
+      $(this).css({
+        "display": "none"
       });
-  // hide按钮操作
+      $(this).siblings().removeClass('block-with-text').find('.hideBtn').css({
+        "display": "inline-block"
+      });
+    });
+    // hide按钮操作
     $(".hideBtn").click(function () {
       // console.log(11);
-      $(this).css({"display":"none"});
-      $(this).parent().addClass('block-with-text').siblings().css({"display":"block"});
+      $(this).css({
+        "display": "none"
       });
+      $(this).parent().addClass('block-with-text').siblings().css({
+        "display": "block"
+      });
+    });
 
     // end
-    };
+  };
 
   // 评论区图片功能
   $.fn.revFunction = function () {
@@ -94,12 +134,12 @@
     $(".revImg").on("click", "li", function () {
       // 获取当前 li 的 eq
       var index = $(this).index(),
-      // 获取对应大图区域
-            par = $(this).parent().parent(),
-      // 获取大图li的长度
-       liLength = par.find(".revbigImg li").length,
-      // 初始化旋转度数为0
-         degNum = 0;
+        // 获取对应大图区域
+        par = $(this).parent().parent(),
+        // 获取大图li的长度
+        liLength = par.find(".revbigImg li").length,
+        // 初始化旋转度数为0
+        degNum = 0;
       // 相应的大图区域展示
       par.find(".greyArea").fadeIn(400);
       par.find(".revbigImgArea").fadeIn(400);
@@ -136,22 +176,22 @@
 
       // 左旋转
       $(".leftRotate").click(function () {
-        degNum = (degNum + 90)%360;
+        degNum = (degNum + 90) % 360;
         // console.log(degNum);
         par.find(".revbigImg li").eq(index).find("img").css({
-          "transform" : "translate(-50%,-50%) rotate("+(-degNum)+"deg)"
+          "transform": "translate(-50%,-50%) rotate(" + (-degNum) + "deg)"
         });
-        });
+      });
       // testend
 
       // 右旋转
       $(".rightRotate").click(function () {
-        degNum = (degNum + 90)%360;
+        degNum = (degNum + 90) % 360;
         // console.log(degNum);
         par.find(".revbigImg li").eq(index).find("img").css({
-          "transform" : "translate(-50%,-50%) rotate("+degNum+"deg)"
+          "transform": "translate(-50%,-50%) rotate(" + degNum + "deg)"
         });
-        });
+      });
       //end
     });
 
@@ -165,70 +205,74 @@ $(function () {
   // 获取当前屏幕的宽度
   const window_width = $(window).width();
 
-if (window_width >= 1280) {
-  jq('#demo1').banqh({
-    box:"#demo1",//总框架
-    pic:"#ban_pic1",//大图框架
-    pnum:"#ban_num1",//小图框架
-    prev_btn:"#prev_btn1",//小图左箭头
-    next_btn:"#next_btn1",//小图右箭头
-    pop_prev:"#prev2",//弹出框左箭头
-    pop_next:"#next2",//弹出框右箭头
-    prev:"#prev1",//大图左箭头
-    next:"#next1",//大图右箭头
-    pop_div:"#demo2",//弹出框框架
-    pop_pic:"#ban_pic2",//弹出框图片框架
-    pop_xx:".pop_up_xx",//关闭弹出框按钮
-    mhc:".mhc",//朦灰层
-    autoplay:false,//是否自动播放
-    interTime:5000,//图片自动切换间隔
-    delayTime:400,//切换一张图片时间
-    pop_delayTime:400,//弹出框切换一张图片时间
-    order:0,//当前显示的图片（从0开始）
-    picdire:true,//大图滚动方向（true为水平方向滚动）
-    mindire:true,//小图滚动方向（true为水平方向滚动）
-    min_picnum:5,//小图显示数量
-    pop_up:true//大图是否有弹出框
-  });
-
-  $(".mhc").click(function () {
-    $(".mhc").css({"display":"none"});
-    $(".pop_up").css({"display":"none"});
+  if (window_width >= 1280) {
+    jq('#demo1').banqh({
+      box: "#demo1", //总框架
+      pic: "#ban_pic1", //大图框架
+      pnum: "#ban_num1", //小图框架
+      prev_btn: "#prev_btn1", //小图左箭头
+      next_btn: "#next_btn1", //小图右箭头
+      pop_prev: "#prev2", //弹出框左箭头
+      pop_next: "#next2", //弹出框右箭头
+      prev: "#prev1", //大图左箭头
+      next: "#next1", //大图右箭头
+      pop_div: "#demo2", //弹出框框架
+      pop_pic: "#ban_pic2", //弹出框图片框架
+      pop_xx: ".pop_up_xx", //关闭弹出框按钮
+      mhc: ".mhc", //朦灰层
+      autoplay: false, //是否自动播放
+      interTime: 5000, //图片自动切换间隔
+      delayTime: 400, //切换一张图片时间
+      pop_delayTime: 400, //弹出框切换一张图片时间
+      order: 0, //当前显示的图片（从0开始）
+      picdire: true, //大图滚动方向（true为水平方向滚动）
+      mindire: true, //小图滚动方向（true为水平方向滚动）
+      min_picnum: 5, //小图显示数量
+      pop_up: true //大图是否有弹出框
     });
 
-  $(".typeColor").colorTurnBigImg("505");
+    $(".mhc").click(function () {
+      $(".mhc").css({
+        "display": "none"
+      });
+      $(".pop_up").css({
+        "display": "none"
+      });
+    });
 
-} else {
-  jq('#demo1').banqh({
-    box:"#demo1",//总框架
-    pic:"#ban_pic1",//大图框架
-    pnum:"#ban_num1",//小图框架
-    prev_btn:"#prev_btn1",//小图左箭头
-    next_btn:"#next_btn1",//小图右箭头
-    pop_prev:"#prev2",//弹出框左箭头
-    pop_next:"#next2",//弹出框右箭头
-    prev:"#prev1",//大图左箭头
-    next:"#next1",//大图右箭头
-    pop_div:"#demo2",//弹出框框架
-    pop_pic:"#ban_pic2",//弹出框图片框架
-    pop_xx:".pop_up_xx",//关闭弹出框按钮
-    mhc:".mhc",//朦灰层
-    autoplay:false,//是否自动播放
-    interTime:5000,//图片自动切换间隔
-    delayTime:400,//切换一张图片时间
-    pop_delayTime:400,//弹出框切换一张图片时间
-    order:0,//当前显示的图片（从0开始）
-    picdire:true,//大图滚动方向（true为水平方向滚动）
-    mindire:true,//小图滚动方向（true为水平方向滚动）
-    min_picnum:5,//小图显示数量
-    pop_up:false//大图是否有弹出框
-  });
+  } else {
+    jq('#demo1').banqh({
+      box: "#demo1", //总框架
+      pic: "#ban_pic1", //大图框架
+      pnum: "#ban_num1", //小图框架
+      prev_btn: "#prev_btn1", //小图左箭头
+      next_btn: "#next_btn1", //小图右箭头
+      pop_prev: "#prev2", //弹出框左箭头
+      pop_next: "#next2", //弹出框右箭头
+      prev: "#prev1", //大图左箭头
+      next: "#next1", //大图右箭头
+      pop_div: "#demo2", //弹出框框架
+      pop_pic: "#ban_pic2", //弹出框图片框架
+      pop_xx: ".pop_up_xx", //关闭弹出框按钮
+      mhc: ".mhc", //朦灰层
+      autoplay: false, //是否自动播放
+      interTime: 5000, //图片自动切换间隔
+      delayTime: 400, //切换一张图片时间
+      pop_delayTime: 400, //弹出框切换一张图片时间
+      order: 0, //当前显示的图片（从0开始）
+      picdire: true, //大图滚动方向（true为水平方向滚动）
+      mindire: true, //小图滚动方向（true为水平方向滚动）
+      min_picnum: 5, //小图显示数量
+      pop_up: false //大图是否有弹出框
+    });
 
-  $(".typeColor").colorTurnBigImg("360");
-  
-}
+  }
 
- 
+
+  window_width >= 1024 ? $(".typeColor").colorTurnBigImg("505") : $(".typeColor").colorTurnBigImg("360");
+
+  // 根据获取的poa灵活处理右侧的高度
+  $(".rightSell").judgeRightHeight();
 
   // 实现li点击之后获取红色边框应用
   $(".typeColor").borderRed(".typeColor");
@@ -272,7 +316,7 @@ if (window_width >= 1280) {
   // 评论区分页特效
   $(".num").click(function () {
     $(this).addClass("currentSel").siblings().removeClass("currentSel");
-    });
+  });
 
   // 评论区函数
   $(".revImg").revFunction();
@@ -283,19 +327,25 @@ if (window_width >= 1280) {
     $(".greyArea").fadeOut(400);
   });
 
-// 缺货时候
-if (!$(".proChoose").height()) {
-  $(".buyArea").css({"display":"none"});
-  $(".ComingSoon").css({"display":"block"});
-} else {
-  $(".ComingSoon").css({"display":"none"});
-}
+  // 缺货时候
+  if (!$(".proChoose").height()) {
+    $(".buyArea").css({
+      "display": "none"
+    });
+    $(".ComingSoon").css({
+      "display": "block"
+    });
+  } else {
+    $(".ComingSoon").css({
+      "display": "none"
+    });
+  }
 
-$(".ComWishlist span").click(function () {
-  $(this).toggleClass('colD533');
+  $(".ComWishlist span").click(function () {
+    $(this).toggleClass('colD533');
   });
 
-// 轮播图
+  // 轮播图
   // 轮播图插件内容
   $('.flexslider').flexslider({
     animation: "slide",
@@ -304,11 +354,11 @@ $(".ComWishlist span").click(function () {
     itemMargin: 20,
     minItems: 2,
     maxItems: 5,
-    slideshow:false,
-    directionNav:true,
-    nextText:"",
-    prevText:"",
-    start: function(slider){
+    slideshow: false,
+    directionNav: true,
+    nextText: "",
+    prevText: "",
+    start: function (slider) {
       $('body').removeClass('loading');
     }
   });
@@ -317,8 +367,9 @@ $(".ComWishlist span").click(function () {
   // test area
 
 
-  
+
+
   // test end
 
-// 入口函数end
+  // 入口函数end
 });
