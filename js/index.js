85d72e753f25d3d7c49956e63e628b5cf6b96898
罗培斌 <luopeibin@banggood.com>
2018-01-17 18:47:52 +0800

    首页导航栏效果更新。

--- js/index.js
+++ js/index.js
@@ -2,15 +2,92 @@
  * @Author: Robyn 
  * @Date: 2017-12-19 11:32:28 
  * @Last Modified by: Robyn
- * @Last Modified time: 2018-01-10 10:53:31
+ * @Last Modified time: 2018-01-17 18:20:02
  */
+
+// 函数封装
+;(function ($) {
+  $.fn.turnColorDark = function () {
+    $(".index_header .nav_bar .nLi > a").css({
+      "color": "#333"
+    });
+    $(".index_header .nav_user a").css({
+      "color": "#333"
+    });
+    $(".index_header .nav_user .search").css({
+      "background-position": "-30px 0"
+    });
+        // 静态时候
+    $(".logo img").attr("src", "../images/logo.png");
+        // 加入php时候
+    // $(".logo img").attr("src", $('.logo').attr('data-path')+"logo.png");
+    };
+
+  $.fn.turnColorWhite = function () {
+    $(".index_header .nav_bar .nLi > a").css({
+      "color": "#fff"
+    });
+    $(".index_header .nav_user a").css({
+      "color": "#fff"
+    });
+    $(".index_header .nav_user .search").css({
+      "background-position": "0 0"
+    });
+    // 静态时候
+    $(".logo img").attr("src", "../images/logo_01.png");
+    // 加入php时候
+    // $(".logo img").attr("src", $('.logo').attr('data-path')+"logo_01.png");
+    };
+
+    // 轮播图插件
+  $.fn.slider = function () {
+    var currentNum,Be_currentLi,currentIndex,currentLi;
+    $(".flexslider").flexslider({
+      animation: "slide",
+      slideshowSpeed: 5000,
+      start: function (slider) {
+        if (!$(".slides li").eq(1).hasClass("dark")) {
+          $(".index_header").turnColorDark();
+        }
+      },
+      before : function (slider) {
+         currentNum = slider.currentSlide + 2;
+         Be_currentLi = $(".slides li").eq(currentNum);
+        if (Be_currentLi.hasClass("dark")) {
+          $(".index_header").turnColorWhite();
+        } else {
+          $(".index_header").turnColorDark();
+        }   
+        },
+      after:function (slider) {
+         currentIndex = slider.currentSlide + 1;
+         currentLi = $(".slides li").eq(currentIndex);
+        if (currentLi.hasClass("dark")) {
+          $(".index_header").turnColorWhite();
+        } else {
+          $(".index_header").turnColorDark();
+        }
+        
+        $(window).scroll(function () {
+          const _height = $(this).scrollTop();
+          if (_height >= 300) {
+            if (currentLi.hasClass("white")) {
+              $(".header_box").css('backgroundColor', 'rgba(255, 255, 255, .8)');
+            }
+          }
+          });
+        
+        },
+    });
+    };
+  
+  })(jQuery);
 
 $(function () {
 
-  // 轮播图插件内容
-    $(".flexslider").flexslider({
-      animation: "slide"
-    });
+  // 轮播图展示
+  $(".flexslider").slider();
+
 
   // 返回顶部按钮区域
   $(window).scroll(function () {
@@ -28,35 +105,36 @@ $(function () {
   // 返回顶部按钮区域结束
 
   // // 加载更多
-// start
-(function () {
-  var showMoreNChildren = function ($children, n) {
-    //显示某jquery元素下的前n个隐藏的子元素
-    var $hiddenChildren = $children.filter(":hidden");
-    var cnt = $hiddenChildren.length;
-    for (var i = 0; i < n && i < cnt; i++) {
-      $hiddenChildren.eq(i).show();
-    }
-    return cnt - n; //返回还剩余的隐藏子元素的数量
-  }
-
-  //对页中现有的class=showMorehandle的元素，在之后添加显示更多条，并绑定点击行为
-  $(".showMoreNChildren").each(function () {
-    var pagesize = $(this).attr("pagesize") || 10;
-    var $children = $(this).children();
-    if ($children.length > pagesize) {
-      for (var i = pagesize; i < $children.length; i++) {
-        $children.eq(i).hide();
+  // start
+  (function () {
+    var showMoreNChildren = function ($children, n) {
+      //显示某jquery元素下的前n个隐藏的子元素
+      var $hiddenChildren = $children.filter(":hidden");
+      var cnt = $hiddenChildren.length;
+      for (var i = 0; i < n && i < cnt; i++) {
+        $hiddenChildren.eq(i).show();
       }
-
-      $("<div class='loadMoreBox df-jcai-center'><div class='loadMore'><span class='loadMoreC'>Load More +</span></div></div>").insertAfter($(this)).click(function () {
-        if (showMoreNChildren($children, pagesize) <= 0) {
-          //如果目标元素已经没有隐藏的子元素了，就隐藏“点击更多的按钮条”
-          $(this).hide();
-        };
-      });
+      return cnt - n; //返回还剩余的隐藏子元素的数量
     }
-  });
-})();
+
+    //对页中现有的class=showMorehandle的元素，在之后添加显示更多条，并绑定点击行为
+    $(".showMoreNChildren").each(function () {
+      var pagesize = $(this).attr("pagesize") || 10;
+      var $children = $(this).children();
+      if ($children.length > pagesize) {
+        for (var i = pagesize; i < $children.length; i++) {
+          $children.eq(i).hide();
+        }
+
+        $("<div class='loadMoreBox df-jcai-center'><div class='loadMore'><span class='loadMoreC'>Load More +</span></div></div>").insertAfter($(this)).click(function () {
+          if (showMoreNChildren($children, 2*pagesize) <= 0) {
+            //如果目标元素已经没有隐藏的子元素了，就隐藏“点击更多的按钮条”
+            $(this).hide();
+          };
+        });
+      }
+    });
+  })();
+
   // end
 })
