8347e6a9d5a08942ff5a62a0f18ac72215b7c4c8
罗培斌 <luopeibin@banggood.com>
2018-02-02 12:16:20 +0800

    重置密码界面添加判断逻辑

---
+++ js/RegisterPwdReset.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: Robyn 
+ * @Date: 2018-02-02 11:51:13 
+ * @Last Modified by: Robyn
+ * @Last Modified time: 2018-02-02 12:09:29
+ */
+
+ function resetPwd(resetNewPwdID,resetCfmPwdID) {
+   var reset_new_pwd = $(resetNewPwdID).val(),
+      reset_cfm_pwd = $(resetCfmPwdID).val();
+      if (reset_new_pwd == "" || reset_cfm_pwd == "") {
+        $(".alertBox").html("please enter password");return;
+      }
+      
+      if (reset_new_pwd != reset_cfm_pwd) {
+        $(".alertBox").html("Confirm Password not match new Password!");return;
+      }
+
+      // var data = "";
+      // $.ajax({
+      //   type:"post",
+      //   dataType:"json",
+      //   data:data,
+      //   url:"",
+      //   beforeSend:function () {
+      //     $(".alertBox").html("Processing...");
+      //     },
+      //   success:function (res) {
+      //     $(".alertBox").html("Reset Successfully");
+      //     }
+      //     if (res.status == true) {
+      //       $("#ResetPwd").val("");
+      //       $("#ResetConfirmPwd").val("");
+      //     }
+      // })
+   }
+
+   $(function () {
+     $(".sub").click(function () {
+      resetPwd("#ResetPwd","ResetConfirmPwd");
+       });
+     });
